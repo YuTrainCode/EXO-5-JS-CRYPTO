39591e631e22d26d3ceb9df9b9bc7e737cca5ad5
Iuliana <iuliana.subbota@gmail.com>
2025-03-12 18:03:59 +0100

    part 4

--- crypto_lib_subbota_iuliana.js
+++ crypto_lib_subbota_iuliana.js
@@ -42,3 +42,29 @@ function count_words_by(string_of_words, letter) {
     }
     return count;
 }
+
+//Partie 4
+
+//function est une voyelle
+function is_a_vowel(letter) {
+
+    for (let i = 0; i < vowels.length; ++i){
+        if (vowels[i] === letter){
+            return true;       }
+    }
+
+    return false;  
+}
+
+function count_vowels(string_of_words) {
+
+    let counter = 0;
+
+    for (let i = 0; i < string_of_words.length; ++i) {
+        if (is_a_vowel(string_of_words.charAt(i))) {
+            counter++;
+
+        }
+    }
+    return counter;
+}
