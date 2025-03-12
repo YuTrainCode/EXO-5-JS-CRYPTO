b73e8fed96f79743b2b7bd439b2a8d1f61ad0a09
Iuliana <iuliana.subbota@gmail.com>
2025-03-12 18:03:07 +0100

    part 2

--- crypto_lib_subbota_iuliana.js
+++ crypto_lib_subbota_iuliana.js
@@ -4,3 +4,22 @@ function what_are_words_worth() {
     console.log(nom.length);
     return confirm("Le mot " + nom + " contient " + nom.length + " caracteres!");
 }
+
+//Partie 2
+function count_words(string_of_words) {
+
+    let count = 0; //pour compter les mots
+    insideWord = false;
+
+    for (let i = 0; i < string_of_words.length; ++i) {
+        if (string_of_words.charAt(i) !== ' ') {
+            if (!insideWord) {
+                count++;
+                insideWord = true;
+            }
+        } else {
+            insideWord = false;
+        }
+    }
+    return count;
+}
